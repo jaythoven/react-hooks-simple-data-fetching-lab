a8af74b7f697034c2b7d9da37fb613597f376128
Jonathan Aguilar <j.aguilar0390@gmail.com>
2022-05-24 16:31:52 -0400

    commit tree ec1ae835bd0695f319304ffbb37692e96f28b4e7

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [image, setImage] = useState(null)
+    // const [loading, setLoading] = useState(true)
+    
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(data => {
+            setImage(data.message)
+        });
+        // if we look at the API link, each image is stored in message key!
+    }, []);
+    // we use [] dependency to have image render only once
+    
+    if (!image) return <p>Loading...</p>;
+    return <img src={image} alt="A Random Dog" />
+
+    // return (
+    //     <div>
+    //         { loading ? <p>"Loading..."</p> : <img>{setLoading}</img> }
+    //     </div>
+    // )
+}
+
+export default App;
